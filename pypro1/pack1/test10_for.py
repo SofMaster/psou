7df1331e47d7025f39b03f87943b3c68085ba902
SofMaster <95130489+SofMaster@users.noreply.github.com>
2022-03-29 17:27:31 +0900

    add test10_for

---
+++ pypro1/pack1/test10_for.py
@@ -0,0 +1,50 @@
+# 반복문 for와 range()
+# range(초기치, 목적치, 증가치) : 수열 생성 함수
+print(list(range(1,6,1)))
+print(list(range(1,6)))
+print(set(range(1,6)))
+print(tuple(range(1,6)))
+print(list(range(6))) #한개만 적는다면 목적치가 된다.
+print(list(range(0, 6)))
+print(list(range(1, 11, 2)))
+print(list(range(-10, -100, -20)))
+
+print()
+for i in range(6):
+    print(i, end = ' ')
+    
+print()
+for i in range(1,10):       # java : for(int i=1; i<=10; i++){}
+    print('{0}*{1}={2}'.format(2,i,i*2), end = ' ')
+    
+print()
+tot = 0
+for i in range(1, 11):
+    tot += i
+    
+print('합은 ' + str(tot))
+print('합은 ' + str(sum(range(1,11))))
+
+# 참고 : n-gram : 문자열에서 n개의 연속된 요소를 추출하기
+# 문자 단위 2 - gram
+text = 'hello'
+
+for i in range(len(text)):
+    print(text[i:i+3])
+
+print()
+# 단어 단위
+text = 'this is python program'
+words = text.split()
+print(words)
+
+for i in range(len(words) - 1):
+    print(words[i], words[i + 1])
+
+# 문1) 2 ~ 9 단 모두 출력
+
+# 문2) 1 ~ 100 사이의 정수 중 3의 배수이면서 5의 배수의 합 출력
+
+# 문3) 주사위를 두 번 던져서 나온 숫자들의 합이 4의 배수가 되는 경우만 출력
+# 예) 1 3
+# 예) 2 2
