90ddc2953ebda5c23c509a0b08bfe3738d8c5ec8
SofMaster <95130489+SofMaster@users.noreply.github.com>
2022-03-31 17:44:22 +0900

    add day4_ex

---
+++ pypro1/examples/day4_ex.py
@@ -0,0 +1,27 @@
+'''
+https://cafe.daum.net/flowlife/RUrO/24
+
+커피 자판기 프로그램
+class Machine
+    cupCount = 1
+    def showData()
+
+class CoinIn
+    coin
+    change
+    def culc(cupCount)
+    
+조건 
+입력자료는 키보드를 사용
+커피는 한 잔에 200원
+100원 넣고 커피를 요구하면 요금 부족 메시지 출력
+400원 넣고 2잔 요구하면 두 잔 출력
+500언 넣고 1잔 요구하면 300원 반납
+
+----------------------
+출력 형태
+동전을 입력하세요: 400
+몇 잔을 원하세요 : 2
+커피 2잔과 잔돈 0원
+
+'''
