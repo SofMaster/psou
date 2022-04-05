e6d52e7d131994e4b62a2ad19cd14eb941fad376
SofMaster <95130489+SofMaster@users.noreply.github.com>
2022-04-05 17:43:50 +0900

    update day6_ex

--- pypro1/examples/day6_ex.py
+++ pypro1/examples/day6_ex.py
@@ -1,48 +1,46 @@
 # 사번과 이름을 입력하여 로그인에 성공하면 
 # buser,jikwon : join 하여
-# 사번, 직원명, *부서명, 부서전화, 연봉, 성별 출력
+# 사번, 직원명, 부서명, 부서전화, 연봉, 성별 출력
+# jik_no,jik_name,buser_name,buser_tel,jikwon_pay, jikwon_gen
 # ex) 1, 홍길덩, 영업부, 123-1234, 12345, 남
 
 import MySQLdb
-
-import pickle                           # mydb.dat 읽어오기
-with open('mydb.dat', 'rb') as obj:
-    config = pickle.load(obj) 
+import pickle   
+with open('mydb.dat' , 'rb') as obj: 
+    config = pickle.load(obj)
     
-    
-def company():
+def findInfo():
     try:
-        conn = MySQLdb.connect(**config)    # dict type를 받을때는 **
+        conn = MySQLdb.connect(**config) 
         cursor = conn.cursor()
         
-        sabun_no = input('사번 입력 : ')
-        jikwon_name = input('이름 입력 : ')
+        jikwon_no = input('사번 입력:')
+        jikwon_name= input('이름 입력:')
         jikwon_name1 = "'" + jikwon_name + "'"
+        
         sql = """
-            select jikwon_no,jikwon_name,buser_name,buser_tel,jikwon_pay, jikwon_gen
+            select jikwon_no, jikwon_name, buser_name, buser_tel, jikwon_pay, jikwon_gen
             from jikwon j
-            INNER JOIN buser b ON j.
-            where buser_num={0}
-        """.format(sabun_no,jikwon_name)
-        # print(sql)
+            INNER JOIN buser b ON j.buser_num = b.buser_no
+            where jikwon_no = {0} and jikwon_name = {1}
+        """.format(jikwon_no, jikwon_name1)
         
         cursor.execute(sql)
         
         datas = cursor.fetchall()
-        # print(datas)
-        # print(len(datas))
+
         if len(datas) == 0:
-            print(buser_no + '번 부서는 없어요!')
+            print('없어요')
             return
-        for jikwon_no,jikwon_name,buser_name,buser_tel,jikwon_pay, jikwon_gen in datas:
-            print(jikwon_no,jikwon_name,buser_name,buser_tel,jikwon_pay, jikwon_gen)
-            
+        
+        for jikwon_no, jikwon_name, buser_name, buser_tel, jikwon_pay, jikwon_gen in datas:
+            print(jikwon_no, jikwon_name, buser_name, buser_tel, jikwon_pay, jikwon_gen)
         
     except Exception as e:
-        print('err : ', e)
+        print('error:', e)
     finally:
         cursor.close()
         conn.close()
         
 if __name__ == '__main__':
-    company()
+    findInfo()
