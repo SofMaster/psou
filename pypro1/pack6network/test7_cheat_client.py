1259d4dd0031cbd0be1f34e5109a99f91d46c73e
SofMaster <95130489+SofMaster@users.noreply.github.com>
2022-04-06 09:37:59 +0900

    update test7_cheat_client

--- pypro1/pack6network/test7_cheat_client.py
+++ pypro1/pack6network/test7_cheat_client.py
@@ -9,15 +9,15 @@ def handle(socket):
         data = socket.recv(1024)
         if not data:continue
         print(data.decode('UTF_8'))
+        
+sys.stdout.flush()  # 파이썬의 표준 출력은 자동으로 버퍼링이 된다.
 
-sys.stdout.flush()  # 파이썬의 표준 출력은 버퍼링이 된다.
-
-name = input('채팅명 입력 : ')
+name = input('채팅명 입력 :')
 cs = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 cs.connect(('127.0.0.1', 5555))
 cs.send(name.encode('UTF_8'))
 
-th = threading.Thread(target = handle, args = (cs,))
+th = threading.Thread(target = handle, args =(cs,))
 th.start()
 
 while True:
@@ -25,5 +25,5 @@ while True:
     sys.stdout.flush()
     if not msg:continue
     cs.send(msg.encode('UTF_8'))
-    
-cs.close()    
+
+cs.close()
