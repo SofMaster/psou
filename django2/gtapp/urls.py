3b21b759163659c8808809e483d7dd869e71ad3b
SofMaster <95130489+SofMaster@users.noreply.github.com>
2022-04-07 21:02:33 +0900

    add urls.py django2,2_ex,3_session,4_shop

---
+++ django2/gtapp/urls.py
@@ -0,0 +1,9 @@
+# main urls.py로 부터 위임받은 urls
+from django.urls import path
+from gtapp import views
+
+urlpatterns = [
+    path('insert', views.insertFunc),
+    # path('insertok', views.insertokFunc), 
+    
+]
