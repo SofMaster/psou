d51b2e47fc86a35fbf79fb0083be146e472c14c1
SofMaster <95130489+SofMaster@users.noreply.github.com>
2022-04-20 18:13:44 +0900

    add pan4bs

--- pypro3/anal2/pan7bs.py
+++ pypro3/anal2/pan7bs.py
@@ -53,3 +53,93 @@ print(soup2.find(id='title').string)
 print(soup2.find(class_='our').string)
 print(soup2.find(attrs={'class':'our'}).string)
 print(soup2.find(attrs={'id':'title'}).string)
+
+print('\nfind_all(), findAll() 사용 ---------')
+html_page3 = """
+<html>
+<body>
+<h1 id = "title">제목 태그</h1>
+<p>웹문서 읽기</p>
+<p id = "my" class="our">파이썬 라이브러리 사용</p>
+<div>
+  <a href="https://www.naver.com">네이버</a>
+  <a href="https://www.daum.net">다음</a>
+</div>
+</body>
+</html>
+"""
+
+soup3 = BeautifulSoup(html_page3, 'lxml')    #lxml : parser(해석기)
+print(soup3.find_all(['a']))
+print(soup3.find_all('a'))
+print(soup3.find_all(['a','p']))
+print(soup3.findAll(['a','p']))
+
+links = soup3.find_all('a')
+for i in links:
+    href = i.attrs['href']
+    text = i.string
+    print(href, ' ', text)
+    
+print()
+import re
+links2 = soup3.find_all(href = re.compile(r'^https'))  # ^https : https로 시작하는 정규 표현식
+print(links2)
+
+print('\n셀렉터(css의 selector) ---')
+html_page4 = """
+<html>
+<body>
+<div >
+    first div
+</div>
+<div id = "hello">
+    <b>파이썬 만세</b>
+    <a href="https://www.kbs.com">kbs</a>
+    <a href="https://www.mbc.com">mbc</a>
+    <span>
+          <a href="https://www.tvn.com">tvn</a>
+    </span>
+</div>
+<span>
+    <a href="https://www.mbc.com">mbc</a>
+</span>
+<ul class = "world">
+    <li>안녕</li>
+    <li>반가워</li>
+</ul>
+<div id = "hi" class = "good">
+  second div
+  <a href="https://www.ytn.com">ytn</a>
+</div>
+</body>
+</html>
+"""
+soup4 = BeautifulSoup(html_page4, 'lxml')
+aa = soup4.select_one("div")        # 단수 선택
+print(aa)
+print('---')
+bb = soup4.select_one("div#hello")
+print(bb)
+print('^^^')
+cc = soup4.select_one("div#hello > a")
+print(cc)
+print(cc.string)
+
+print('~~~~~~~~~~~')
+dd = soup4.select("div#hello > a")  # 복수 선택(직계 자식)
+print(dd)
+ee = soup4.select("div#hello a")  # 복수 선택(자손)
+print(ee)
+ff = soup4.select("ul.world > li")
+for k in ff:
+    print('li : ', k.string)
+    
+print()
+msg = list()   # msg = []
+for k in ff:
+    msg.append(k.string)
+    
+import pandas as pd
+df = pd.DataFrame(msg,columns = ['자료'])
+print(df)
